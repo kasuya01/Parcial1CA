bcd65cea109de79206c04cd615075e2d33062d53
Julio Castillo <julio@c0128-00pc68.salud.gob.sv>
2016-01-08 14:56:56 +0000

    Reporte registro diario pagina completa

--- LAB/Reportes/ReporteRegistroDiario/export_to_excel.php
+++ LAB/Reportes/ReporteRegistroDiario/export_to_excel.php
@@ -33,44 +33,55 @@ $resultgetExamnResult =$obj->getRegistroDiario($idarea, $d_fechadesde, $lugar);
         border-collapse: collapse;
         font-size: 8px;
     }
+    .header_white{
+        border-right-color: white;
+        font-size: 8px;
+    }
+    .td_data{
+        height: 45;
+    }
+
+    
 </style>
 
 <?php
 
 $rslt="";
-$rslt .= "<table border='1' class='tabla'>
-    
+$rslt .= "<table border=0px width='100%'>
         <tr>
-            <td colspan='2' align='left' width='20%'><img id='Image1' style='WIDTH: 80px; HEIGHT: 55px' height='86' src='../../../Imagenes/escudo.png' width='210' name='Image1'></td>
-            <td align='center' colspan='7' width='60%' class='Estilo5'>
+            <td class='header_white' colspan='2' align='left'><img id='Image1' style='WIDTH: 80px; HEIGHT: 55px' height='86' src='../../../Imagenes/escudo.png' width='210' name='Image1'></td>
+            <td class='header_white' align='center' colspan='7' class='Estilo5'>
                 <p><strong>".$row_estab['nombre']."</strong></p>
                 <p><strong>REGISTRO DIARIO DE ACTIVIDADES DE LABORATORIO</strong></p>
                 <p><strong>&Aacute;REA DE ".htmlentities($row_area['nombre'])." </strong></p>
                 <p><strong>F E C H A:  ".$d_fechadesde[8].$d_fechadesde[9]."/".$d_fechadesde[5].$d_fechadesde[6]."/".$d_fechadesde[0].$d_fechadesde[1].$d_fechadesde[2].$d_fechadesde[3]."</strong></p>
             </td>
-            <td colspan='1' align='right' width='10%'><img id='Image3' style='WIDTH: 110px; HEIGHT: 55px' height='86' src='../../../Imagenes/paisanito.png' width='210' name='Image3'></td>
+            <td class='header_white' colspan='1' align='right'><img id='Image3' style='WIDTH: 110px; HEIGHT: 55px' height='86' src='../../../Imagenes/paisanito.png' width='210' name='Image3'></td>
         </tr>
-        
+        </table>
+        <table border='1' class='tabla' width='100%'>
           <tr>
-          <td>&nbsp;</td>
-          <td colspan='4' align='center'>IDENTIFICACION DE LA PERSONA</td>
-          <td>&nbsp;</td>
-          <td colspan='2' align='center'>PROCEDIMIENTO</td>
-          <td colspan='2' align='center'>RESULTADO</td>
+              <td >&nbsp;</td>
+              <td colspan='4' align='center'>IDENTIFICACION DE LA PERSONA</td>
+              <td rowspan=2 width=25px>&nbsp;</td>
+              <td colspan='2' align='center'>PROCEDIMIENTO</td>
+              <td colspan='2' align='center'>RESULTADO</td>
           <tr>
 
           <tr>
-          <td width='50px'>No. ORDEN</td>
-          <td width='300px' align='center'>NOMBRE</td>
-          <td>EDAD</td>                
-          <td>SEXO</td>
-          <td>EXPEDIENTE</td>
-          <td>PROCED</td>
-          <td align='center'>NOMBRE</td>
-          <td>CODIGO</td>
-          <td width='300px' align='center' colspan=2>COMENTARIO</td>
+              <td width='50px'>No. ORDEN</td>
+              <td width='100px' align='center'>NOMBRE</td>
+              <td width=25px>EDAD</td>                
+              <td width=25px>SEXO</td>
+              <td width='75px'>EXPEDIENTE</td>
+              <td width=25px>PROCED</td>
+              <td width=100px align='center'>NOMBRE</td>
+              <td width='30px'>CODIGO</td>
+              <td width='300px' align='center' colspan=2>COMENTARIO</td>
           <tr>";
 
+          $i=0;
+          $l=10;
     while ($row = pg_fetch_array($resultgetExamnResult)){
         $edad = str_replace('years','a<br>',$row['edad']);
         $edad = str_replace('year','a<br>',$edad);
@@ -79,9 +90,9 @@ $rslt .= "<table border='1' class='tabla'>
         $edad = str_replace('days','d<br>',$edad);
         $edad = str_replace('day','d<br>',$edad);
         $rslt .= "<tr>
-                  <td align='center'>".$row['numeromuestra']."</td>
+                  <td class='td_data' align='center'>".$row['numeromuestra']."</td>
                   <td>".strtoupper($row['paciente'])."</td>
-                  <td width=30px>".$edad."</td>
+                  <td>".$edad."</td>
                   <td align='center'>".substr($row['sexo'],0,1)."</td>
                   <td>".$row['idnumeroexp']."</td>
                   <td align='center'>".$row['idservicio']."</td>
@@ -89,7 +100,23 @@ $rslt .= "<table border='1' class='tabla'>
                   <td align='center'>".$row['idestandar']."</td>
                   <td colspan=2>&nbsp;</td>
                   </tr>";
+        $i++;
     }
+    for ($j=0; $j<$l; $j++){
+        $rslt .= "<tr>
+                  <td class='td_data' align='center'>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td align='center'>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td align='center'>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td align='center'>&nbsp;</td>
+                  <td colspan=2>&nbsp;</td>
+                  </tr>";
+    }
+    
+    
     $rslt .= "</table>";
 echo $rslt;
 echo "<input type='button' onclick='imprimir()' value='Imprimir' id='imprimir'>";
